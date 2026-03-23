4006c7bdba207298402d473b6ef80a5306c5153e
Marcelo Miky Mine <marcelo.mine@gmail.com>
2026-03-23 09:45:54 -0300

    Correção na documentação do README.md, ajuste no arquivo config.py, add arquivo 2026-01-bb.ofx (#114)
    * corrige typo em README.md, alterei config.py para funcionar o beancount do extrato do BB, adicionei arquivo 2026-01-bb.ofx na pasta extratos
    
    * Update config.py
    
    * add financeiro/2026-01.beancount
    
    * include row with 2026-01.beancount
    
    ---------
    
    Co-authored-by: miky_somos_educacao <marcelo.mine-net@somos.parceirosedu.com.br>

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from beancount.ingest.importers import ofx
 
 CONFIG = [
-    ofx.Importer(r"78126\-6", "Assets:Bancos:BB", balance_type=ofx.BalanceType.NONE),
+    ofx.Importer(r"^78126$", "Assets:Bancos:BB", balance_type=ofx.BalanceType.NONE), # Há versões que no arquivo ofx do app do BB está com o dígito 6: "78126\-6"
 ]
